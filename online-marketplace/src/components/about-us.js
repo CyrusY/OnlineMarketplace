d2f1797065ff36ebf2470d7a049d4a3237c707ca
choy0001 <choy0001@gmail.com>
2021-03-19 18:45:02 +0800

    brian

--- online-marketplace/src/components/about-us.js
+++ online-marketplace/src/components/about-us.js
@@ -7,7 +7,10 @@ render() {
       <div>
           <h3>About Us Page! </h3>
           <br></br>
-          <p>Content</p>
+          <center><p>“One person’s trash is another person’s treasure”.</p></center> 
+          <center><p>Online Marketplace is an online platform for users to list and view new or used goods for trading. </p></center>
+          <center><p>The application allows buyers and sellers to effectively communicate and schedule meets up with easy to understand procedures and features such as the chat box and reminder.</p></center> 
+          <center><p>Through this platform, we hope to bring convencies for traders and deter online trading scams and fraud.</p></center>
       </div>
     )
   }
