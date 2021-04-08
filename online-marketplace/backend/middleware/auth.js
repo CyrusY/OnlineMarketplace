da31c396c8cf09666c8449f805927fe92e45123e
jimmy008002 <59361111+jimmy008002@users.noreply.github.com>
2021-04-08 17:34:47 +0800

    s

--- online-marketplace/backend/middleware/auth.js
+++ online-marketplace/backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 const auth = (req, res, next) => {
     try {
-        const token = req.header("Authorization")
+        const token = req.header("Authorization") //伺服器端取得 JWT
         if(!token) return res.status(400).json({msg: "Invalid Authentication."})
 
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
