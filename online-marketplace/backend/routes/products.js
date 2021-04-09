d313979a7b0861d9b711c15725116ddca2b9b7c4
CyrusY <cyrusy1819@gmail.com>
2021-04-10 02:51:44 +0800

    “9/4update_3.3”

--- online-marketplace/backend/routes/products.js
+++ online-marketplace/backend/routes/products.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();     // express routers
 const multer = require('multer');   // upload photo
 
-let Products = require('../models/products.model');
+let Products = require("../models/products.model");
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, '../public/uploads/');
+    callback(null, 'public/uploads/');
   },
   filename: (req, file, callback) => {
     callback(null, file.originalname);
@@ -19,7 +19,7 @@ const upload = multer({storage: storage});
 router.get("/", (req, res) => {       //  get request: 'localhost:3000/product/' case
     Products.find()       //get list of Product in mongodb atlas
       .then(product => res.json(product))     //after find, return users in json format (from DB)
-      .catch(err => res.status(400).json('Error: ' + err));       // return status 400 if error 
+      .catch(err => res.status(400).json("Error: " + err));       // return status 400 if error 
 });
 
 router.post("/add", upload.single("productPhoto"), (req, res) => {   // post request
@@ -31,17 +31,19 @@ router.post("/add", upload.single("productPhoto"), (req, res) => {   // post req
     productPhoto: req.file.originalname
   });
 
+  console.log(req.file.originalname);
+
   // validation
   newProduct.save()        // save the new user to DB
-     .then(() => res.json('Product uploaded!'))        // return "User added" if add success
-     .catch(err => res.status(400).json('Error: ' + err));   // return error if failed
+     .then(() => res.json("Product uploaded!"))        // return "User added" if add success
+     .catch(err => res.status(400).json("Error: " + err));   // return error if failed
 });
 
 router.get("/:id",(req, res) => {        
   // id object was created by mongo automatically since object created , get request, return that product photo by that id
   Products.findById(req.params.id)          // findByID
     .then((product) => res.json(product))       // return as json
-    .catch((err) => res.status(400).json('Error: ' + err));
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.put("/update/:id", upload.single("productName"),(req, res) => {    //update data of the object by id
@@ -56,16 +58,16 @@ router.put("/update/:id", upload.single("productName"),(req, res) => {    //upda
       /* end */
 
     product.save()
-      .then(() => res.json('Product Updated!'))
-      .catch(err => res.status(400).json('Error: ' + err));
+      .then(() => res.json("Product Updated!"))
+      .catch(err => res.status(400).json("Error: " + err));
   })
-  .catch(err => res.status(400).json('Error: ' + err));
+  .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.delete("/:id",(req, res) => {     //pass in object id, delete request, delete that object by id
   Products.findByIdAndDelete(req.params.id)       //findByIdAndDelete
-    .then(() => res.json('Product deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => res.json("Product deleted."))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
