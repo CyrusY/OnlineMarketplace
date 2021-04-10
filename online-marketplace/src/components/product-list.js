05f190b10b2bac1dae47c2af41067ebc608d8609
CyrusY <cyrusy1819@gmail.com>
2021-04-10 14:50:56 +0800

    “10/4update_1.2”

---
+++ online-marketplace/src/components/product-list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ProductList extends Component {
+
+render() {
+    return (
+      <div>
+          <p>ProductList component</p>
+      </div>
+    )
+  }
+}
