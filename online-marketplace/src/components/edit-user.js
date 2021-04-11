db8a4888035bff536c446910964ed7c896da251c
ALEX <alexlsk2719@gmail.com>
2021-04-11 21:06:43 +0800

    updated,but still hv 3 problems, details in code comment.

--- online-marketplace/src/components/edit-user.js
+++ online-marketplace/src/components/edit-user.js
@@ -4,6 +4,11 @@ import axios from 'axios';
 import './edit-user.css';
 
 //Only Update User displayname and description
+/* three problems now: 
+1. textbox cant edit
+2. cant update
+3. get can depends on log-in user
+*/
 
 export default class EditUser extends Component {
 
@@ -14,16 +19,86 @@ export default class EditUser extends Component {
           user:{}
         }
 
+        this.onChangeDisplayName = this.onChangeDisplayName.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
       }
     
-      componentDidMount() {
-        axios.get('http://localhost:5000/users/60519a60167898652cc4723e')
+    componentDidMount() {
+        axios.get('http://localhost:5000/users/60519af6167898652cc47243')
           .then(res => {
             console.log(res)
             this.setState({ user: res.data });
           })
           .catch(error => { console.log(error) })
       }
+    
+    onChangeDisplayName(e) {
+        this.setState({
+            displayName: e.target.value
+        })
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    validate = () => {
+        let usernameError = "";
+        let emailError = "";
+        let passwordError1 = "";
+        let passwordError2 = "";
+        let passwordError3 = "";
+        let passwordError4 = "";
+
+        if (this.state.username.length <3) {
+            usernameError = "Username need to be at least 3 characters long."
+        }
+
+        if (usernameError) {
+            this.setState({usernameError});
+            return false;
+        }
+
+        return true;
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const user = {
+            displayName: this.state.displayName,
+            description: this.state.description,
+        }
+
+        const isValid = this.validate();
+
+        if(isValid) {
+            console.log(user);
+            axios.post('http://localhost:5000/users/update/', user)
+            .then(res => console.log(res.data));
+            alert('Account Updated! Welcome, ' + this.state.displayName + '!');
+
+            /* clear form and error */
+            this.setState({
+                displayName: '',
+                description: '',
+
+
+                usernameError: '',
+                emailError: '',
+                passwordError1: '',
+                passwordError2: '',
+                passwordError3: '',
+                passwordError4: '',
+                validPasswordError: ''
+            });
+            // window.location = '/login';
+        }
+    }
 
       
 
@@ -43,7 +118,8 @@ render() {
                                     placeholder='Enter display Name'
                                     className="form-control form-group"
                                     value={this.state.user.username}
-                                    onChange={this.state.user.username}
+                                    onChange={this.onChangeDisplayName}
+                                    // onChange={this.state.user.username}
                                 />
                             </div>
 
