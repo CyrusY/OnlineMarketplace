6dd9993ba316c8ff38a394398361953fc5b5d52f
lkwsunny <62560458+lkwsunny@users.noreply.github.com>
2021-04-11 17:46:36 +0800

    change navbar to hook

--- online-marketplace/src/components/productList/product-list.js
+++ online-marketplace/src/components/productList/product-list.js
@@ -7,7 +7,7 @@ import './product-list.css';
 
 export default class ProductList extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -17,37 +17,38 @@ export default class ProductList extends Component {
 
   componentDidMount() {
     axios.get('http://localhost:5000/products/')
-      .then(res => { console.log(res)
-        this.setState({ products: res.data});
+      .then(res => {
+        console.log(res)
+        this.setState({ products: res.data });
       })
-      .catch (error => {console.log(error)})
+      .catch(error => { console.log(error) })
   }
 
-render() {
-  const {products}=this.state 
+  render() {
+    const { products } = this.state
     return (
       <div className="product-list-container">
         <div className="grid-contianer">
-        {
-          products.length ?
-          products.map( product =>
-          <MainContainer key = {product._id}>
-            <div className="card-container">
-            <span className = "tag-container">
-              {product.productName}
-            </span>
-            <div
-              style={{fontSize:16, fontWeight: "bold"}}>HK${product.price}
-              <span style={{fontSize:12, color: 'GrayText'}}> • {product.condition}</span> 
-            </div>
-            <div>posted at: {new Date(product.postDate).getDate() }</div>
-            {/* <div>Description: {product.productDescription}</div> */}
-            </div>
-            
-          </MainContainer>) : null
-        }
+          {
+            products.length ?
+              products.map(product =>
+                <div key={product._id} className="card-container">
+                 
+                    <span className="tag-container">
+                      {product.productName}
+                    </span>
+                    <div
+                      style={{ fontSize: 16, fontWeight: "bold" }}>HK${product.price}
+                      <span style={{ fontSize: 12, color: 'GrayText' }}> • {product.condition}</span>
+                    </div>
+                    <div>posted at: {new Date(product.postDate).getDate()}</div>
+                    {/* <div>Description: {product.productDescription}</div> */}
+                 
+
+                </div>) : null
+          }
         </div>
-        
+
       </div>
     );
   }
